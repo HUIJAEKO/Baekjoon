805c4d87370c547dfe447703bad64c773024e9c6
HUIJAEKO <xrhgmlwox@naver.com>
2024-10-08 14:25:06 +0900

    [Bronze V] Title: 직사각형, Time: 168 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+
+        int area = x*y;
+
+        System.out.println(area);
+    }
+}
