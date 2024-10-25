74fbebb32b1e6863352b4a333a4465e8a254b305
HUIJAEKO <xrhgmlwox@naver.com>
2024-10-25 10:56:34 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,24 +1,14 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
         int answer = 0;
-        if(date1[0] < date2[0]){
-            answer = 1;
-        }else if(date1[0] == date2[0]){
-            if(date1[1] < date2[1]){
+        for(int i=0; i<date1.length; i++){
+            if(date1[i]<date2[i]){
                 answer = 1;
-            }else if(date1[1] == date2[1]){
-                if(date1[2] < date2[2]){
-                    answer = 1;
-                }else if(date1[2] == date2[2]){
-                    return answer;
-                }else{
-                    return answer;
-                }
-            }else{
-                return answer;
+                break;
+            }else if(date1[i]>date2[i]){
+                answer=0;
+                break;
             }
-        }else{
-            return answer;
         }
         return answer;
     }
