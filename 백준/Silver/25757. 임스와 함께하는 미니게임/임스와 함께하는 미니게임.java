c6e247b849825dec5f7966ada6e03c41a890566b
HUIJAEKO <xrhgmlwox@naver.com>
2025-01-31 11:35:19 +0700

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 236 ms, Memory: 27352 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        String game = st.nextToken();
+
+        HashSet<String> set = new HashSet<>();
+        for(int i=0; i<N; i++){
+            set.add(br.readLine());
+        }
+
+        if(game.equals("Y")){
+            System.out.println(set.size());
+        }else if(game.equals("F")){
+            System.out.println(set.size() / 2);
+        }else{
+            System.out.println(set.size() / 3);
+        }
+    }
+}
