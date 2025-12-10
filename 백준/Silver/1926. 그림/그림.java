ae78a2a2763bc5cb3686444517369611d613835a
HUIJAEKO <xrhgmlwox@naver.com>
2025-12-10 14:49:26 +0900

    [Silver I] Title: 그림, Time: 396 ms, Memory: 43768 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visited;
+
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int count = 0;      
+        int maxArea = 0;    
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    count++;
+                    int area = bfs(i, j);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+
+        System.out.println(count);
+        System.out.println(maxArea);
+    }
+
+    static int bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        int area = 1;
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int cx = now[0];
+            int cy = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                    continue;
+
+                // 방문 안 했고 그림(1)이라면
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    q.add(new int[]{nx, ny});
+                    area++;
+                }
+            }
+        }
+
+        return area;
+    }
+}
