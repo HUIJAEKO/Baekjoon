808f909ebcfc9c54a99e4857b326481bf7ca426e
HUIJAEKO <xrhgmlwox@naver.com>
2026-01-12 21:32:11 +0900

    [Silver III] Title: 가희와 키워드, Time: 2152 ms, Memory: 237392 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<String> keywords = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            keywords.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String line = br.readLine();
+            String[] used = line.split(",");
+
+            for (String word : used) {
+                keywords.remove(word); 
+            }
+
+            System.out.println(keywords.size());
+        }
+    }
+}
