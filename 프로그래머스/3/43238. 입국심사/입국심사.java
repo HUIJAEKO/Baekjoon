6f544655e5a981433a19e70c489d5bbbb45eff97
HUIJAEKO <xrhgmlwox@naver.com>
2026-03-20 14:41:00 +0900

    [level 3] Title: 입국심사, Time: 63.87 ms, Memory: 95.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        long left = 1;
+        long right = (long)times[times.length - 1] * n;
+        long answer = right;
+        
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            
+            long people = 0;
+            for (int time : times) {
+                people += mid / time;
+            }
+            
+            if (people >= n) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
